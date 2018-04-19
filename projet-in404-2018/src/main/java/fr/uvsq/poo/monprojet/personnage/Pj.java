ce6d19a11295ee5b21f64e7ee73fc99c06545972
JavaProjet <37223151+JavaProjet@users.noreply.github.com>
2018-04-19 14:49:26 +0200

    Ajout discussions

--- projet-in404-2018/src/main/java/fr/uvsq/poo/monprojet/personnage/Pj.java
+++ projet-in404-2018/src/main/java/fr/uvsq/poo/monprojet/personnage/Pj.java
@@ -51,7 +51,43 @@ public class Pj extends Personnage{
 		if(direction == 'O') this.representation = '<';
 		if(direction == 'E') this.representation = '>';
 	}
-
+	
+	public void discuss(Terrain T){
+		Scanner discute = new Scanner(System.in);
+		int x = T.joueur.position.getX();
+		int y = T.joueur.position.getY();
+		int test=0;
+		for(int j=0; j<T.personnage.size(); j++){
+			int px = T.personnage.get(j).position.getX();
+			int py = T.personnage.get(j).position.getY();
+			if(py == y+1 || py == y-1|| px == x+1 || px == x-1){
+				Random r = new Random();
+				int rand = r.nextInt(2);
+				if(rand == 0){
+					System.out.println("Bonjour aventurier !");
+					System.out.println("1- Bonjour (Partir)");
+					System.out.println("2- Bonjour, avez-vous entendu parler d'un mysterieux objet supposé être dans ces grottes?");
+					int reponse = discute.nextInt();
+					if(reponse == 2){
+						System.out.println("On raconte que les gens de l'ombre garderaient un mysterieux teleporteur, personnellement je n'ai jamais rien vu de tel");
+					}
+				}
+				if(rand == 1){
+					System.out.println("Comment vas-tu aventurier?");
+					System.out.println("1- Ignorer ce personnage");
+					System.out.println("2- Je me mefie des creatures, elles sont nombreuses par ici..");
+					int reponse = discute.nextInt();
+					if(reponse == 2){
+						System.out.println("Tu as raison, reste sur tes gardes. Certains peuvent même casser la roche pour t'attaquer.");
+						System.out.println("Si tu as besoin de te soigner, tu pourras trouver des fioles medicinales par ici");
+					}
+				}
+				test =1;
+			}
+		}
+		if(test == 0) System.out.println("Vous ne pouvez discuter qu'a cote d'un personnage !");
+	}
+	
 	public void addMonnaie(int valeur) {
 		monnaie += valeur;
 	}
