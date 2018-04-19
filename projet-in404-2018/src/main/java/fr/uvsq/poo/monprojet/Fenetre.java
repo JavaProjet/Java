74aa10a9e79c826e3d56dd1741f4f298acfc7305
JavaProjet <37223151+JavaProjet@users.noreply.github.com>
2018-04-19 14:34:05 +0200

    Add files via upload

---
+++ projet-in404-2018/src/main/java/fr/uvsq/poo/monprojet/Fenetre.java
@@ -0,0 +1,52 @@
+package fr.uvsq.poo.monprojet;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.*;
+import java.io.File;
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.net.URL;
+import java.io.IOException;
+
+
+public class Fenetre implements ActionListener{
+	public boolean start = false;
+	public JFrame  F  = new JFrame("Rogue Like");
+	public JPanel P = new JPanel();
+	public JButton jouer = new JButton("Jouer partie");
+	public JButton charger = new JButton("Charger partie");
+	public JLabel titre = new JLabel("Menu Principal", SwingConstants.CENTER);
+	public JLabel fond = new JLabel(new ImageIcon("pic.jpg"));
+	public ImageIcon icone = new ImageIcon("pic.jpg");
+	
+	public Fenetre(){
+		
+		fond.setSize(10,10);
+		jouer.addActionListener(this);
+		charger.addActionListener(this);
+		P.setLayout(new BorderLayout());
+		P.add(titre, BorderLayout.NORTH);
+		P.add(jouer, BorderLayout.WEST);
+		P.add(charger,BorderLayout.EAST);
+		P.add(fond);
+		
+		F.setIconImage(icone.getImage());
+		F.setSize(new Dimension(600,300));
+		F.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        F.setContentPane(P);
+		F.setVisible(true);
+
+	}
+	
+	public void actionPerformed(ActionEvent e){
+		this.start = true;
+	}
+	
+	public boolean getGo(){
+		System.out.print("");
+		if(this.start == true){
+			return this.start;
+		}
+		else return false;
+	}
+}
